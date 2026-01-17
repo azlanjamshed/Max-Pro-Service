40074341a68f12b59aeae6033e6822153ecae319
Azlan Jamshed <152954543+azlanjamshed@users.noreply.github.com>
2026-01-18 01:40:37 +0530

    fix image

--- src/components/Contact/ContactHero.jsx
+++ src/components/Contact/ContactHero.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-
+import hero from "../../assets/contactBanner.jpg";
 const ContactHero = () => {
   return (
     <>
       <section className=" w-full  ">
         {/* Decorative Icons (Optional) */}
-        <img
-          src="src/assets/contactBanner.jpg"
-          alt="hero image"
-          className="w-full h-[80vh]"
-        />
+        <img src={hero} alt="hero image" className="w-full h-[80vh]" />
       </section>
     </>
   );
